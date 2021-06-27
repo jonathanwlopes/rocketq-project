4d0041825679b9a97c17fa3f96d5bd40caf6b1c8
jonathanwlopes <jonathanwlopes93@gmail.com>
2021-06-27 17:14:14 -0300

    update code

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -4,25 +4,43 @@ module.exports = {
   async create(req, res) {
     const db = await Database();
     const pass = req.body.password;
-
     let roomId;
+    let isRoom = true;
 
-    for (let i = 0; i < 6; i++) {
-      i == 0
-        ? (roomId = Math.floor(Math.random() * 10).toString())
-        : (roomId += Math.floor(Math.random() * 10).toString());
+    while (isRoom) {
+      for (let i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString());
+      }
+
+      const roomsExistId = await db.all(`SELECT id FROM rooms `);
+      isRoom = roomsExistId.some((roomsExistId) => roomsExistId === roomId);
+
+      if (!isRoom) {
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+      ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+      )`);
+      }
     }
-
-    await db.run(`INSERT INTO rooms (
-        id,
-        pass
-    ) VALUES (
-        ${parseInt(roomId)},
-        ${pass}
-    )`);
 
     await db.close(``);
 
     res.redirect(`/room/${roomId}`);
+  },
+
+  async open(req, res) {
+    const db = await Database();
+    const roomId = req.params.room;
+
+    const questions = await db.all(
+      `SELECT * FROM questions WHERE room = ${roomId}`
+    );
+
+    res.render("room", { roomId, questions, questionsRead });
   },
 };
